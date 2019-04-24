0a9f30d64a46864ef9d8c46f1368f9672df0ec2f
anhnt2989 <anhnt2989@gmail.com>
2019-04-24 14:40:36 +0700

    nop bai

---
+++ diem_sinh_vien.js
@@ -0,0 +1,16 @@
+let X;
+let Y;
+let Z;
+    X=prompt("Nhập điểm Vật lý");
+    Y=prompt("Nhập điểm Hoá học");
+    Z=prompt("Nhập điểm Sinh học");
+let x=parseFloat(X);
+    let y=parseFloat(Y);
+    let z=parseFloat(Z);
+
+    let a=(x+y+z)/3;
+
+    document.write("Điểm trung bình là:"+a);
+
+
+
